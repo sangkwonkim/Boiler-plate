9218d7dd7324d127494121788235a9b1588c7388
sangkwonkim <sangkwon2406@naver.com>
2021-09-14 08:47:21 +0900

    9월 14일 노드 강의 5강까지 들음

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://sangkwon:qkdtl21@cluster0.imyq0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    }).then(() => console.log('MongoDB Connceted...'))
+
 app.get('/', (req, res) => res.send('hello world!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
