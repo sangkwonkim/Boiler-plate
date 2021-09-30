2b64dbd77d16f58faef5eaba7b94a596589bf29e
sangkwonkim <sangkwon2406@naver.com>
2021-09-30 22:43:26 +0900

    boilerplate 다시 시작하기

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.json());;
+app.use(express.urlencoded({extended: true}));
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://sangkwon:qkdtl21@cluster0.imyq0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    }).then(() => console.log('MongoDB Connceted...'))
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=> console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('hello world!'))
+
+app.post('/register', (req, res) => {
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ succedd: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -29,4 +29,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+module.exports = { User }
