85a99dcdd90fd2defdcc850d765050f4ae39865b
sangkwonkim <sangkwon2406@naver.com>
2021-10-15 08:42:17 +0900

    redux 설정

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.less';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducers';
+const createStroeWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider
+    store={createStroeWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && 
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+  
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
