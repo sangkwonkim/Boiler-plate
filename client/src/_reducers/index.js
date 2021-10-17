400425eb27b8448b0c5190695ca170f2195adc74
sangkwonkim <sangkwon2406@naver.com>
2021-10-18 08:59:38 +0900

    리덕스 추가 학습

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "redux"; // 리덕스 스토어에서 다양한 리듀서를 콤바인리듀서를 통해서 관리한다.
 // import user from './user_reducer';
 
 const rootReducer = combineReducers({
--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,14 @@ import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk'
 import Reducer from './_reducers';
-const createStroeWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
+const createStroeWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+// 미들웨어를 이용해서 객체 액션만 받는 스토어가 promise와 function를 받을 수 있도록 해준다.
 ReactDOM.render(
 
-  <Provider
-    store={createStroeWithMiddleware(Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && 
+  <Provider //어플리케이션이랑 리덕스랑 연결을 시킨다.
+    store={createStroeWithMiddleware(Reducer, // 포로바이더에 스토어를 넣어준다.
+      window.__REDUX_DEVTOOLS_EXTENSION__ && // 데브툴을 이용해서 리덕스를 편하게 이용할 수 있다.
       window.__REDUX_DEVTOOLS_EXTENSION__()
       )}
   >
