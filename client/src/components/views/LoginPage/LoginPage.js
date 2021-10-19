a5793e9d9eddccc8c087b27a43bf3003044984b3
sangkwonkim <sangkwon2406@naver.com>
2021-10-20 08:48:53 +0900

    로그인 기능 다시 학습하기

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    const onEmailHandler = (event) => {
+        setEmail(event.target.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.target.value)
+    }
+    const onsubmitHandler = (event) => {
+
+    }
     return (
-        <div>
-            LoginPage
+        <div style={{display: 'flex', justifyContent: ' center', alignItems: 'center', width: "100%", height: '100vh'}}>
+            <form style={{display:'flex', flexDirection:'column'}} onSubmit={onsubmitHandler}>
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler}></input>
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler}></input>
+                <br />
+                <button type='submit'>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
